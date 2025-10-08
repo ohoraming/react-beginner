0d463249a112619574a3f2291c2266db973ebaab
minji <ohora.ming@gmail.com>
2025-10-08 17:20:42 +0900

    메인 컨텐츠 영역 구현(skeleton)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {AppFooter, AppHeader} from "@/components/common";
 import {ThemeProvider} from "./components/theme-provider";
 import {AppSidebar} from "@/components/common/AppSidebar.tsx";
+import {SkeletonHotTopic, SkeletonNewTopic} from "@/components/skeleton";
 
 function App() {
     return (
@@ -12,7 +13,38 @@ function App() {
                         {/* 카테고리 사이드바 */}
                         <AppSidebar/>
                         {/* 토픽 컨텐츠 */}
-                        <section></section>
+                        <section className="flex-1 flex flex-col gap-12">
+                            <div className="w-full flex flex-col gap-6">
+                                <div className="flex flex-col gap-1">
+                                    <div className="flex items-center gap-2">
+                                        <img src="./assets/fire.gif" alt="@IMG" className="w-7 h-7" />
+                                        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">핫토픽</h4>
+                                    </div>
+                                    <p className="text-md text-muted-foreground">지금 가장 주목받는 주제들을 살펴보고 다양한 관점의 인사이트를 얻어가세요.</p>
+                                </div>
+                                <div className="grid grid-cols-4 gap-6">
+                                    <SkeletonHotTopic/>
+                                    <SkeletonHotTopic/>
+                                    <SkeletonHotTopic/>
+                                    <SkeletonHotTopic/>
+                                </div>
+                            </div>
+                            <div className="w-full flex flex-col gap-6">
+                                <div className="flex flex-col gap-1">
+                                    <div className="flex items-center gap-2">
+                                        <img src="./assets/writing.gif" alt="@IMG" className="w-7 h-7" />
+                                        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">뉴토픽</h4>
+                                    </div>
+                                    <p className="text-md text-muted-foreground">새로운 시선으로 새로운 이야기를 시작하세요. 지금 바로 당신만의 토픽을 작성해보세요.</p>
+                                </div>
+                                <div className="grid grid-cols-2 gap-6">
+                                    <SkeletonNewTopic/>
+                                    <SkeletonNewTopic/>
+                                    <SkeletonNewTopic/>
+                                    <SkeletonNewTopic/>
+                                </div>
+                            </div>
+                        </section>
                     </main>
                 </div>
                 <AppFooter/>
